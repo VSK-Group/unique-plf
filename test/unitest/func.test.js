23487da3174e8ed51f93f2eab7fdcb6625d9ac32
khanhney <khanhney.dev@gmail.com>
2023-02-06 11:49:15 +0700

    setup: testw

--- test/unitest/func.test.js
+++ test/unitest/func.test.js
@@ -4,7 +4,6 @@ const reduce    = require('../../utils/reduce');
 test('SUM: adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-
 test('REDUCE: adds 2 - 1 to equal 1', () => {
     expect(reduce(2, 1)).toBe(1);
   });
